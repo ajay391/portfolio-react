5f4a53390a34705fc1241624c0ffe7c83880cf80
ajay391 <prajay391@gmail.com>
2023-09-19 21:59:39 +0530

    updates education and experience

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,71 +1,120 @@
-import React ,{useState}from 'react'
-
-// import { Col, Row,  } from 'reactstrap';
-import {MdOutlineArrowRight} from 'react-icons/md'
-import './Education.css'
+import React, { useState } from 'react';
+import './Education.css';
 
 const Education = () => {
+  const [activeDiv, setActiveDiv] = useState(1);
+  const [focusedButton, setFocusedButton] = useState(1);
 
-   const [activeDiv, setActiveDiv] = useState(null);
-const [focusedButton, setFocusedButton] = useState(null);
+  const showDiv = (divNumber, buttonNumber) => {
+    setActiveDiv(divNumber);
+    setFocusedButton(buttonNumber);
+  };
 
- const showDiv = (divNumber, buttonNumber) => {
- setActiveDiv(divNumber);
-setFocusedButton(buttonNumber);
- };
-
-const divs = [
- { id: 1, content: 'MG University' , course :'BCA', year:'2018 - 2021'},
- { id: 2, content: 'SNDP HSS , Udayamperoor', course :'Computer Science', year:'2016 - 2018'},
-{ id: 3, content: 'SVHS , Thripunithura', course :'SSLC', year:'2015'},
- ];
+  const divs = [
+    { id: 1, content: 'MG University, Kottayam', course: 'BCA', year: '2018 - 2021' },
+    {
+      id: 2,
+      content: 'SNDP HSS , Udayamperoor',
+      course: 'Computer Science',
+      year: '2016 - 2018',
+    },
+    { id: 3, content: 'SVHS , Thripunithura', course: 'SSLC', year: '2015' },
+  ];
 
   return (
-    <div id='education'>
-      <section className='edu_section'>
-      <h2>Education</h2>
-      <div className='edu_container'>
-        <div className='btn_container'>
-       
-          <button
-          className={focusedButton === 1 ? 'button-focused' : ''}
-          onClick={() => showDiv(1, 1)}
-          >
-          Degree
-          </button>
-          <button
-          className={focusedButton === 2 ? 'button-focused' : ''}
-          onClick={() => showDiv(2, 2)}
-          >
-          Plus Two
-          </button>
-          <button
-          className={focusedButton === 3 ? 'button-focused' : ''}
-          onClick={() => showDiv(3, 3)}
-          >
-          SSLC
-          </button>
-        
+    <div id="education">
+      <section className="edu_section">
+        <h2>Education</h2>
+        <p>
+          Education has been the foundation of my journey, equipping me with
+          essential skills and knowledge. Explore my academic achievements below.
+        </p>
+        <div className="edu_container">
+          {/* <div className="checkbox_container">
+            <label className='radio'>
+              <input
+                type="checkbox"
+                checked={focusedButton === 1}
+                onChange={() => showDiv(1, 1)}
+              />
+              <span className="name">Degree</span> 
+            </label>
+            <label className='radio'>
+              <input
+                type="checkbox"
+                checked={focusedButton === 2}
+                onChange={() => showDiv(2, 2)}
+              />
+             <span className="name">Plus Two</span> 
+            </label>
+            <label className='radio'>
+              <input
+                type="checkbox"
+                checked={focusedButton === 3}
+                onChange={() => showDiv(3, 3)}
+              />
+             <span className="name">SSLC</span> 
+            </label>
+          </div> */}
+          <div class="edu_btn_container">
+              <div class="tabs">
+              <input
+                type="radio"
+                name="tabs"
+                id="radio-1"
+                checked={focusedButton === 1}
+                onChange={() => showDiv(1, 1)}
+              />
+              <label class="tab" for="radio-1">Degree</label>
+              <input
+                type="radio"
+                name="tabs"
+                id="radio-2"
+                checked={focusedButton === 2}
+                onChange={() => showDiv(2, 2)}
+              />
+		          <label class="tab" for="radio-2">Plus Two</label>
+              <input
+                type="radio"
+                name="tabs"
+                id="radio-3"
+                checked={focusedButton === 3}
+                onChange={() => showDiv(3, 3)}
+              />
+                <label class="tab" for="radio-3">SSLC</label>
+                <span class="glider"></span>
+              </div>
+            </div>
+            <div class="edu_card">
+              <div class="tools">
+                <div class="circle">
+                  <span class="red box"></span>
+                </div>
+                <div class="circle">
+                  <span class="yellow box"></span>
+                </div>
+                <div class="circle">
+                  <span class="green box"></span>
+                </div>
+                <div class="edu_text">
+                  <span class="">education.jsx</span>
+                </div>
+              </div>
+              <div class="card__content">
+                {divs.map((div) => (
+                  <div key={div.id} className={div.id === activeDiv ? '' : 'hidden'}>
+                    <h5 className="edu_course">{div.course}</h5>
+                    <h6>{div.content}</h6>
+                    <p>{div.year}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          
         </div>
-
-        <div className='edu_content'>
-          {divs.map((div) => (
-          <div key={div.id} className={div.id === activeDiv ? '' : 'hidden'}>
-            <h5>{div.course}</h5>
-            <h6>{div.content}</h6>
-            <p>{div.year}</p>
-          </div>
-          ))}
-          </div>
-        </div>
-        
-       </section>
-   
+      </section>
     </div>
+  );
+};
 
-    
-  )
-}
-
-export default Education
-
+export default Education;
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,18 +1,78 @@
 import React from 'react';
 import './Experience.css';
-
-
+// import { Chrono } from "react-chrono";
+// import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 
 const Experience = () => {
+
+  // const items = [{
+  //   title: "2012-2013",
+  //   cardTitle:<h2>Candorcode</h2>,
+  //   cardSubtitle:
+  //     "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+  // },{
+  //   title: "May 1940",
+  //   cardTitle: "Dunkirk",
+  //   cardSubtitle:
+  //     "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+  // }];
  
   return (
     <div id='experience'>
       <section className='exp_section'>
         <h2 className='exp_heading'>Experience</h2>
+        <p>I bring a wealth of experience in web development and design, having worked on a variety of projects for clients from different industries. Explore my journey and notable achievements below.</p>
         
-        <p>
-          Your relevant content here...
-        </p>
+          {/* <Chrono items={items} 
+            mode="VERTICAL_ALTERNATING" 
+            slideShow
+            slideItemDuration={4500}
+            slideShowType="reveal"
+            hideControls={true}
+            theme={{
+              primary: '#5eead4', // Customize the primary color
+              secondary: '#000', // Customize the secondary color
+              cardBg: 'black', // Customize the card background color
+              cardForeColor: 'black', // Customize the card text color
+            }}
+            /> */}
+
+
+        <div class="timeline_container">
+        
+          <div class="timeline-block timeline-block-right">
+              <div class="marker"></div>
+              <div class="timeline-content">
+                <h3>Password Publications Ptv. Ltd.</h3>
+                <h5>Junior Web Developer</h5>
+                <h6>Jan 2023 - Present</h6>
+                <p>Experienced web developer proficient in HTML, CSS, and PHP, dedicated to crafting user-centered online experiences.</p>
+              </div>
+          </div>
+        
+          <div class="timeline-block timeline-block-right">
+              <div class="marker"></div>
+              <div class="timeline-content">
+                <h3>Candorcode Ptv. Ltd.</h3>
+                <h5>Junior Software Engineer</h5>
+                <h6>April 2022 - Dec 2022</h6>
+                <p>Junior software engineer skilled in ReactJS and Django, focused on delivering innovative solutions and enhancing user interactions.</p>
+              </div>
+          </div>
+        
+          <div class="timeline-block timeline-block-right">
+              <div class="marker"></div>
+              <div class="timeline-content">
+                <h3>Govt. Law College, Ernakulam</h3>
+                <h5>Data Entry Operator</h5>
+                <h6>Aug 2021 - Nov 2021</h6>
+                <p>Data entry operator experienced in Excel, ensuring accurate and efficient data management.</p>
+              </div>
+          </div>
+        
+          
+        </div>
+
       </section>
     </div>
   );
